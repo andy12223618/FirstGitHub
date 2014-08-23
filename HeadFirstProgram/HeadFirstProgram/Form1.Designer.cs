2592d38fc7544abe284c36b20dae8f324750e47b
andy12223618 <donglite@126.com>
2014-08-23 21:02:33 +0800

    第一个版本测试
    使用程序进行第一个版本测试

---
+++ HeadFirstProgram/HeadFirstProgram/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace HeadFirstProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Label contactIDLabel;
+            this.contectDBDataSet1 = new HeadFirstProgram.ContectDBDataSet1();
+            this.contactTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.contactTableTableAdapter = new HeadFirstProgram.ContectDBDataSet1TableAdapters.ContactTableTableAdapter();
+            this.tableAdapterManager = new HeadFirstProgram.ContectDBDataSet1TableAdapters.TableAdapterManager();
+            this.contactTableBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.contactTableBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.contactIDTextBox = new System.Windows.Forms.TextBox();
+            contactIDLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.contectDBDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactTableBindingNavigator)).BeginInit();
+            this.contactTableBindingNavigator.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contectDBDataSet1
+            // 
+            this.contectDBDataSet1.DataSetName = "ContectDBDataSet1";
+            this.contectDBDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // contactTableBindingSource
+            // 
+            this.contactTableBindingSource.DataMember = "ContactTable";
+            this.contactTableBindingSource.DataSource = this.contectDBDataSet1;
+            // 
+            // contactTableTableAdapter
+            // 
+            this.contactTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.ContactTableTableAdapter = this.contactTableTableAdapter;
+            this.tableAdapterManager.UpdateOrder = HeadFirstProgram.ContectDBDataSet1TableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // contactTableBindingNavigator
+            // 
+            this.contactTableBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.contactTableBindingNavigator.BindingSource = this.contactTableBindingSource;
+            this.contactTableBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.contactTableBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.contactTableBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.contactTableBindingNavigatorSaveItem});
+            this.contactTableBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.contactTableBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.contactTableBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.contactTableBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.contactTableBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.contactTableBindingNavigator.Name = "contactTableBindingNavigator";
+            this.contactTableBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.contactTableBindingNavigator.Size = new System.Drawing.Size(284, 25);
+            this.contactTableBindingNavigator.TabIndex = 0;
+            this.contactTableBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "移到第一条记录";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "移到上一条记录";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "位置";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "当前位置";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(32, 17);
+            this.bindingNavigatorCountItem.Text = "/ {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "总项数";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "移到下一条记录";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "移到最后一条记录";
+            this.bindingNavigatorMoveLastItem.Click += new System.EventHandler(this.bindingNavigatorMoveLastItem_Click);
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "新添";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "删除";
+            // 
+            // contactTableBindingNavigatorSaveItem
+            // 
+            this.contactTableBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.contactTableBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("contactTableBindingNavigatorSaveItem.Image")));
+            this.contactTableBindingNavigatorSaveItem.Name = "contactTableBindingNavigatorSaveItem";
+            this.contactTableBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.contactTableBindingNavigatorSaveItem.Text = "保存数据";
+            this.contactTableBindingNavigatorSaveItem.Click += new System.EventHandler(this.contactTableBindingNavigatorSaveItem_Click);
+            // 
+            // contactIDLabel
+            // 
+            contactIDLabel.AutoSize = true;
+            contactIDLabel.Location = new System.Drawing.Point(74, 59);
+            contactIDLabel.Name = "contactIDLabel";
+            contactIDLabel.Size = new System.Drawing.Size(71, 12);
+            contactIDLabel.TabIndex = 1;
+            contactIDLabel.Text = "Contact ID:";
+            // 
+            // contactIDTextBox
+            // 
+            this.contactIDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.contactTableBindingSource, "ContactID", true));
+            this.contactIDTextBox.Location = new System.Drawing.Point(151, 56);
+            this.contactIDTextBox.Name = "contactIDTextBox";
+            this.contactIDTextBox.Size = new System.Drawing.Size(100, 21);
+            this.contactIDTextBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(contactIDLabel);
+            this.Controls.Add(this.contactIDTextBox);
+            this.Controls.Add(this.contactTableBindingNavigator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contectDBDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contactTableBindingNavigator)).EndInit();
+            this.contactTableBindingNavigator.ResumeLayout(false);
+            this.contactTableBindingNavigator.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ContectDBDataSet1 contectDBDataSet1;
+        private System.Windows.Forms.BindingSource contactTableBindingSource;
+        private HeadFirstProgram.ContectDBDataSet1TableAdapters.ContactTableTableAdapter contactTableTableAdapter;
+        private HeadFirstProgram.ContectDBDataSet1TableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator contactTableBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton contactTableBindingNavigatorSaveItem;
+        private System.Windows.Forms.TextBox contactIDTextBox;
+    }
+}
+
---
+++ HeadFirstProgram/HeadFirstProgram/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HeadFirstProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contactTableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.contactTableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contectDBDataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“contectDBDataSet1.ContactTable”中。您可以根据需要移动或移除它。
+            this.contactTableTableAdapter.Fill(this.contectDBDataSet1.ContactTable);
+
+        }
+
+        private void bindingNavigatorMoveLastItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
